70dff45536b23a8f16cf7dfa6a4dd1d8644468fa
seog14 <36527388+seog14@users.noreply.github.com>
2021-03-27 20:31:44 -0400

    CreateSchedule

--- aws-service/src/main/java/org/senproject/ppapa/api/APICreateSchedule.java
+++ aws-service/src/main/java/org/senproject/ppapa/api/APICreateSchedule.java
@@ -12,6 +12,7 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.senproject.ppapa.dto.Login;
 import org.senproject.ppapa.dto.Response;
+import org.senproject.ppapa.dto.Schedule;
 import org.senproject.ppapa.repository.UserRepository;
 
 import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
@@ -22,10 +23,9 @@ import com.amazonaws.services.cloudwatchevents.model.PutRuleRequest;
 import com.amazonaws.services.cloudwatchevents.model.PutRuleResult;
 import com.amazonaws.services.cloudwatchevents.model.RuleState;
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 
-public class LoginCheck implements RequestStreamHandler {
-
+public class APICreateSchedule {
+	
 	public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
 
 		JSONParser parser = new JSONParser();
@@ -35,26 +35,13 @@ public class LoginCheck implements RequestStreamHandler {
 		try {
 			JSONObject responseBody = new JSONObject();
 			JSONObject event = (JSONObject) parser.parse(reader);
-			context.getLogger().log("LoginCheck invoked " + event);
+			context.getLogger().log("CreateSchedule invoked " + event);
 			if (event.get("body") != null) {
-				Login login = (Login) Login.newInstance(Login.class, (String) event.get("body"));
-				UserRepository repository = new UserRepository();
-				 
-				if (repository.check(login.getUser(), login.getPass(), login.getRole())) {
-					response.setMessage("Success");
-					response.setError("No Error");
-					response.setStatus(1);
-				} else {
-					response.setMessage("Failure");
-					response.setError("No Error");
-					response.setStatus(0);
-				}
-
-			}
-
-			// Test to put rule
-			putCWRule();
-			//
+				Schedule schedule = (Schedule) Schedule.newInstance(Schedule.class, (String) event.get("body"));
+				putCWRule(schedule.getUser(), schedule.getMonth(), schedule.getHour(), schedule.getMinute());
+				response.setMessage("Success");
+				response.setError("No Error");
+				response.setStatus(1);
 			JSONObject headerJson = new JSONObject();
 			headerJson.put("x-custom-header", "my custom header value");
 
@@ -63,6 +50,10 @@ public class LoginCheck implements RequestStreamHandler {
 			responseJson.put("body", response.toString());
 
 		} catch (ParseException pex) {
+			response.setMessage("Failure");
+			response.setError("Error");
+			response.setStatus(0);
+			responseJson.put("body", response.toString());
 			responseJson.put("statusCode", 400);
 			responseJson.put("exception", pex);
 		}
@@ -77,10 +68,35 @@ public class LoginCheck implements RequestStreamHandler {
 
 		// implementation
 	}
-
-	public void handleLogin(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
-
-	}
-
 	
+	public void putCWRule(String name, int month, int hour, int minute ) {
+//		try {
+//		System.setProperty("aws.accessKeyId", "AKIASYDRTNJ5XI2MNP67");
+//		System.setProperty("aws.secretKey", "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");
+
+		AmazonCloudWatchEventsClientBuilder builder = AmazonCloudWatchEventsClientBuilder.standard();
+		AmazonCloudWatchEvents cwe = AmazonCloudWatchEventsClientBuilder.standard().withEndpointConfiguration(
+				new EndpointConfiguration("https://events.us-east-1.amazonaws.com", Regions.US_EAST_1.getName())).build();		
+		
+			PutRuleRequest request = new PutRuleRequest()
+					.withName(name)
+					.withScheduleExpression("cron(" + minute + " " + hour + " * " + month + " ? 2021")
+					.withState(RuleState.ENABLED);
+
+			PutRuleResult response = cwe.putRule(request);
+//			System.out.printf("Successfully created CloudWatch events rule %s with arn %s", response.toString(),
+//					response.getRuleArn());
+			// roleArn, response.ruleArn());
+//		} catch (CloudWatchException e) {
+//			System.err.println(e.awsErrorDetails().errorMessage());
+//			System.exit(1);
+//		}
+	}
+	public void addTarget(String name) {
+		AmazonCloudWatchEventsClientBuilder builder = AmazonCloudWatchEventsClientBuilder.standard();
+		AmazonCloudWatchEvents cwe = AmazonCloudWatchEventsClientBuilder.standard().withEndpointConfiguration(
+				new EndpointConfiguration("https://events.us-east-1.amazonaws.com", Regions.US_EAST_1.getName())).build();
+		
+		Target target = new Target(.)
+	}
 }
---
+++ aws-service/src/main/java/org/senproject/ppapa/dto/Schedule.java
@@ -0,0 +1,43 @@
+package org.senproject.ppapa.dto;
+
+import org.senproject.ppapa.model.JsonModel;
+
+public class Schedule extends JsonModel {
+
+	private String user;
+	private int hour;
+	private int minute;
+	private int month;
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public int getHour() {
+		return hour;
+	}
+
+	public void setHour(int hour) {
+		this.hour = hour;
+	}
+
+	public int getMinute() {
+		return minute;
+	}
+
+	public void setMinute(int minute) {
+		this.minute = minute;
+	}
+
+	public int getMonth() {
+		return month;
+	}
+
+	public void setMonth(int month) {
+		this.month = month;
+	}
+}
