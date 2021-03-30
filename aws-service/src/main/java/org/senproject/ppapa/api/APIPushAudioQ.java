b00a4a53059b5b6132297b878a65f3978816b01b
seog14 <36527388+seog14@users.noreply.github.com>
2021-03-29 22:33:10 -0400

    audio

--- aws-service/src/main/java/org/senproject/ppapa/api/APIPushAudioQ.java
+++ aws-service/src/main/java/org/senproject/ppapa/api/APIPushAudioQ.java
@@ -1,5 +1,83 @@
 package org.senproject.ppapa.api;
 
-public class APIPushAudioQ {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.senproject.ppapa.dto.AudioQ;
+import org.senproject.ppapa.dto.PatientSQS;
+import org.senproject.ppapa.dto.PrescriptionKey;
+import org.senproject.ppapa.dto.Response;
+import org.senproject.ppapa.repository.PatientRepository;
+import org.senproject.ppapa.repository.PrescriptionRepository;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.amazonaws.services.sqs.model.SendMessageRequest;
+
+public class APIPushAudioQ implements RequestStreamHandler {
+	
+	
+
+	public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
+
+		JSONParser parser = new JSONParser();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+		JSONObject responseJson = new JSONObject();
+		Response response = new Response();
+		
+		
+		try {
+			
+			JSONObject responseBody = new JSONObject();
+		JSONObject event = (JSONObject) parser.parse(reader);
+			context.getLogger().log("APIPushAudioQ invoked");
+			if (event.get("body") != null) {
+				AudioQ audioQ = (AudioQ) AudioQ.newInstance(AudioQ.class, (String)event.get("body"));
+				final AmazonSQS sqs1 = AmazonSQSClientBuilder.defaultClient();
+			       
+				SendMessageRequest send_msg_request1 = new SendMessageRequest()
+				        .withQueueUrl("https://sqs.us-east-1.amazonaws.com/245932314163/AudioQ")
+				        .withMessageBody(audioQ.getKey());
+				
+				sqs1.sendMessage(send_msg_request1);
+				
+			}
+			
+			
+
+			JSONObject headerJson = new JSONObject();
+			headerJson.put("x-custom-header", "my custom header value");
+
+			responseJson.put("statusCode", 200);
+			responseJson.put("headers", headerJson);
+			responseJson.put("body", response.toString());
+
+		} catch (ParseException pex) {
+			responseJson.put("statusCode", 400);
+			responseJson.put("exception", pex);
+		}
+
+		OutputStreamWriter writer = new OutputStreamWriter(output, "UTF-8");
+		writer.write(responseJson.toString());
+		writer.close();
+	}
+
+	public void handleGetByParam(InputStream inputStream, OutputStream outputStream, Context context)
+			throws IOException {
+
+		// implementation
+	}
+
+	public void handleLogin(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+
+	}
 }
--- aws-service/src/main/java/org/senproject/ppapa/dto/AudioQ.java
+++ aws-service/src/main/java/org/senproject/ppapa/dto/AudioQ.java
@@ -1,5 +1,17 @@
 package org.senproject.ppapa.dto;
 
-public class AudioQ {
+import org.senproject.ppapa.model.JsonModel;
 
+public class AudioQ extends JsonModel {
+
+	private String key;
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	} 
+	
 }
